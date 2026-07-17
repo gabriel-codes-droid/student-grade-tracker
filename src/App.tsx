cbb7854544ca9289414c177ce33102295286b567
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-07-18 00:58:14 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,120 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState, useEffect } from 'react'
+import type  { Student } from './student'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [students, setStudents] = useState<Student[]>(() => {
+    const storedStudents = localStorage.getItem('students')
+    return storedStudents ? JSON.parse(storedStudents) : []
+  });
+
+  const [name, setName] = useState('');
+  const [grade, setGrade] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('students', JSON.stringify(students));
+  }, [students]);
+
+  const addStudent = () => {
+    if (name.trim() === '' || grade.trim() === '') {
+      alert('Please enter both name and grade.');
+      return;
+    }
+
+    const newStudent: Student = {
+      id: Date.now(),
+      name:name,
+      grade: parseFloat(grade),
+    };
+    setStudents([...students, newStudent]);
+    setName('');
+    setGrade('');
+  };
+  const deleteStudent=(id:Student['id'])=>{
+    setStudents(students=>
+      students.filter(student =>student.id !==id)
+    )
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <h1>Here are each students and there grades</h1>
+      <input 
+      
+      placeholder='Name'
+      value={name}
+      onChange={(e)=> setName(e.target.value)}
 
-      <div className="ticks"></div>
+      />
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <input
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      placeholder='Grades'
+      value={grade}
+      onChange={(e)=> setGrade(e.target.value)}
+      />
+      <button onClick={addStudent}>Add new Student</button>
+
+      <ul>
+        {students.map(student=>(
+          <li key = {student.id}>
+          {student.name} : {student.grade}
+          <button onClick={()=>{deleteStudent(student.id)}}>Delete this student</button>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
